2b58a06ea891723c57b59ba03c314a0f17153556
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-06-02 15:03:57 +0100

    Print console args

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -6,7 +6,11 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
-            
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
         }
+
     }
 }
