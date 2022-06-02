6a5becef58414f7fd6447dd2160c7f54466ca477
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-06-02 15:12:19 +0100

    Add CheckString method to check if given string is a palindrome

--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -8,9 +8,41 @@ namespace Palindrome
         {
             for (int i = 0; i < args.Length; i++)
             {
-                Console.WriteLine(args[i]);
+                Console.WriteLine($"{args[i]} -> {CheckString(args[i])}");
             }
         }
 
+        private static bool CheckString(string s)
+        {
+            bool IsPalindrome(string s)
+            {
+                int check = 0;
+
+                for (int i = 0, j = s.Length - 1; i < s.Length; i++, j--)
+                {
+                    if (s[i] == s[j])
+                    {
+                        check++;
+                    }
+                }
+
+                if (check == s.Length)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            if (s == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (s.Length < 2)
+                return true;
+            
+            return IsPalindrome(s);
+        }
     }
 }
